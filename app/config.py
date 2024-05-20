a7a1563a3ab1ad26f1366b35668f27bb048c828f
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-05-20 13:22:27 -0400

    user model

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
-CORS(app)
+# app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
+# CORS(app)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
